091b7c35f43d65d5d20403b2d67159bf154a845c
Oluwaferanmi-Michael <aechealgr8@gmail.com>
2023-11-18 21:04:22 -0700

    phonebook exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,102 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PhoneBook from './components/Phonebook_form'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchValue, setNewSearchValue] = useState('')
+
+  // add new data event
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    // New Created Data
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    // to prevent duplicate data
+    persons.find(person => {
+      if(newPerson.name === person.name){
+        console.log('alert condition holds true')
+        alert(`${person.name} is already added to the phonebook`)
+      } else {
+        console.log('alert condition holds false')
+        setPersons(persons.concat(newPerson))
+        setNewName('')
+        setNewNumber('')
+      }})
+  }
+
+  // Handle value change in input fields (creating controlled components)
+  const handleChange = () => {
+    setNewName(event.target.value)
+  }
+  const phoneNumberChange = () => {
+    setNewNumber(event.target.value)
+  }
+
+  const searchFunction = () => {
+    setNewSearchValue(event.target.value)
+  }
+
+    
+  const people = persons.filter((person) => searchValue.toLowerCase() === person.name.toLowerCase())
 
   return (
     <>
+    {/* <div>debug: {newName}</div> */}
+
+    <Filter
+      people={people}
+      searchValue={searchValue}
+      searchFunction={searchFunction}
+      ></Filter>
+    
+
+    <br></br>
+      <h2>Phonebook</h2>
+
+      <PhoneBook 
+        submitionHandler={addPerson}
+        nameInputValue={newName}
+        numberInputValue={newNumber}
+        nameChangeHandler={handleChange}
+        numberChangeHandler={phoneNumberChange}></PhoneBook>
+
+      <h2>Numbers</h2>
+      <Numbers persons={persons}></Numbers>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
 export default App
+
+const Numbers = ({persons}) => 
+  <>
+    {persons.map(person => <p key={person.id}>{person.name}, {person.number}</p>)}
+  </>
+
+
+const Filter = ({searchValue, searchFunction, people}) => {
+  return (
+    <>
+      <div>
+        search <input value={searchValue} onChange={searchFunction}></input>
+      </div>
+      {people.map(person => <p key={person.id}>{person.name}</p>)}
+    </>
+  )
+}
---
+++ part2/phonebook/src/components/Phonebook_form.jsx
@@ -0,0 +1,31 @@
+
+const PhoneBook = ({
+    nameInputValue,
+    nameChangeHandler,
+    numberInputValue,
+    numberChangeHandler,
+    submitionHandler
+    }) => {
+
+    return (
+     // Input Form 
+     <>
+       <form name='phonebook input'>
+         <div>
+           name: <input value={nameInputValue} onChange={nameChangeHandler}></input>
+         </div>
+         <div>
+           phone number: <input value={numberInputValue} onChange={numberChangeHandler}></input>
+         </div>
+   
+         {/* Button */}
+         <div>
+           <button type='submit' onClick={submitionHandler}>add</button>
+         </div>
+       </form>
+     </>
+    )
+   }
+
+   
+   export default PhoneBook
